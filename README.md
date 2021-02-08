0cb98e03318aaecfd0a37d96180228f2607a7a34
Mikki <msmikkimonroe@gmail.com>
2021-02-08 15:06:43 -0600

    added firebase

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ npm i -g @ionic/cli
 npm i -g @angular/cli
 npm i -g cordova
 npm i -g cordova-res
+
+
+ionic serve --ssl
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl1MVP3bafzCnWG8RBZNmjLhyveracvsA",
+  authDomain: "authcoreioniccordovafirebase.firebaseapp.com",
+  projectId: "authcoreioniccordovafirebase",
+  storageBucket: "authcoreioniccordovafirebase.appspot.com",
+  messagingSenderId: "80323442868",
+  appId: "1:80323442868:web:53c7115c16873ff91f7fe1"
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
